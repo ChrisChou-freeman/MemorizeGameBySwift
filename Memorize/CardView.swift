8d5c82be6a897f071bf5c78f420007abf28a04e5
ChrisChou-freeman <ChrisChouFreeMan@hotmail.com>
2022-03-28 11:59:12 +0800

    add card view

---
+++ Memorize/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by ChrisChou on 2022/3/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
